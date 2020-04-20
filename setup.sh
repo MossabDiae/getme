194a1c1daec1740325784cdc98c633d8fe9391c0
mossaybo <49007883+mossaybo@users.noreply.github.com>
2020-04-20 21:26:19 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
-cd
+cd ~/
 apt update
-apt install -y inetutils-ping net-tools tmux nano  
+apt install -y inetutils-ping net-tools nano  
 pip install youtube-dl
-
 echo "alias ipe='curl ipinfo.io/ip'
 alias nano='nano -c'
 alias hg='history | grep'
 alias e=exit
 cd" >> .bashrc
-
 source ~/.bashrc
+wget https://github.com/mossaybo/getme/raw/getme-patch-1/geturl https://github.com/mossaybo/getme/raw/getme-patch-1/sendurl
+gpg -d geturl > getme && chmod +x getme
+gpg -d sendurl > getme && chmod +x sendme
+cp -l ~/{getme,sendme} /opt/conda/bin/
 echo READY
