9b66851f98dbee3bfe6abe366f18bde2f62d50be
mossaybo <49007883+mossaybo@users.noreply.github.com>
2020-04-21 00:27:10 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 cd ~/
 apt update
-apt install -y inetutils-ping net-tools nano  
+apt install -y inetutils-ping net-tools nano aria2
 pip install youtube-dl
 echo "alias ipe='curl ipinfo.io/ip'
 alias nano='nano -c'
 alias hg='history | grep'
+alias f='find . -type f -iname'
+alias youtube-dl-mp3='youtube-dl --no-cache-dir -x --audio-format mp3'
+alias youtube-dl-fire='youtube-dl --external-downloader aria2c --external-downloader-args "-x 16 -s 16 -k 1M --file-allocation=none"'
 alias e=exit
 cd" >> .bashrc
 source ~/.bashrc
