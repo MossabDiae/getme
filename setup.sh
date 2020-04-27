734a3de44f9ab538fe2728553ed869c37bfced5b
mossaybo <49007883+mossaybo@users.noreply.github.com>
2020-04-27 22:01:49 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 cd ~/
 apt update
 apt install -y inetutils-ping net-tools nano aria2 rtorrent
-pip install youtube-dl telegram-upload beautifulsoup4
+pip install youtube-dl telegram-upload beautifulsoup4 mega.py
 echo "alias ipe='curl ipinfo.io/ip'
 alias nano='nano -c'
 alias hg='history | grep'
@@ -12,8 +12,11 @@ alias e=exit
 cd" >> .bashrc
 source ~/.bashrc
 wget https://github.com/mossaybo/getme/raw/getme-patch-1/geturl https://github.com/mossaybo/getme/raw/getme-patch-1/sendurl
-gpg -d geturl > getme && chmod +x getme
-gpg -d sendurl > sendme && chmod +x sendme
+gpg -d geturl > getme || gpg -d geturl > getme && chmod +x getme
+gpg -d sendurl > sendme || gpg -d sendurl > sendme && chmod +x sendme
 cp -l ~/{getme,sendme} /opt/conda/bin/
+touch jpslayer && cp -s ~/jpslayer /opt/conda/bin/ && rm jpslayer
+touch tomega && cp -s ~/tomega /opt/conda/bin/ && rm tomega
+echo ohayou > littlebox.txt
 echo READY
 bash
