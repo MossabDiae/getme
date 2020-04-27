c387e4f5f8b4f9084d98a6320fefe14d44a54586
mossaybo <49007883+mossaybo@users.noreply.github.com>
2020-04-27 15:19:42 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 cd ~/
 apt update
 apt install -y inetutils-ping net-tools nano aria2 rtorrent
-pip install youtube-dl telegram-upload
+pip install youtube-dl telegram-upload beautifulsoup4
 echo "alias ipe='curl ipinfo.io/ip'
 alias nano='nano -c'
 alias hg='history | grep'
